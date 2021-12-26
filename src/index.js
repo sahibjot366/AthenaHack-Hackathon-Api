58bed29aea74748651a45abd9b2d0d9eeb3a7eb6
Sahibjot Singh <sahibjot366@gmail.com>
2021-12-26 16:47:22 +0530

    Food list service added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
-const express=require('express');
-const {getLocation}=require('./apis/city')
-const {getWeather}=require('./apis/weather')
-const app=express();
+const express = require("express");
+const { getLocation } = require("./apis/city");
+const { getWeather } = require("./apis/weather");
+const { foodItems } = require("./foodItems");
+const app = express();
 
 app.use(express.json());
 
-app.get('/weather/details',async (req,res)=>{
-    const latitude=req.query.latitude;
-    const longitude=req.query.longitude;
-    try {
-        const locationResponse=await getLocation({latitude,longitude});
-        if(locationResponse.error){
-            throw new Error(locationResponse.error);
-        }
-        const city=locationResponse.city.toString();
-        const weatherResponse=await getWeather(city);
-        if(weatherResponse.error){
-            throw new Error(weatherResponse.error)
-        }
-        res.send({...weatherResponse.data,city});
-    } catch (error) {
-        res.status(500).send(error.message);
+app.get("/weather/details", async (req, res) => {
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+  try {
+    const locationResponse = await getLocation({ latitude, longitude });
+    if (locationResponse.error) {
+      throw new Error(locationResponse.error);
     }
-})
+    const city = locationResponse.city.toString();
+    const weatherResponse = await getWeather(city);
+    if (weatherResponse.error) {
+      throw new Error(weatherResponse.error);
+    }
+    res.send({ ...weatherResponse.data, city });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-app.get('/food/list',async (req,res)=>{
-})
-const port=process.env.PORT;
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}...`)
-})
+app.get("/food/list", async (req, res) => {
+  try {
+    const weather = req.query.weather.toString();
+    const allowedWeathers = ["sunny", "cold", "rainy"];
+    if (allowedWeathers.includes(weather)) {
+      res.send(foodItems[weather]);
+    } else {
+      throw new Error("Food details for given weather not available!");
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
